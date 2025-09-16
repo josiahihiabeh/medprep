aeee019b6277d1e5ce525c7d95ec2751e1ecb0ff
josiahihiabeh <ihiabehojodalejosiah@gmail.com>
2025-09-16 01:01:50 +0100

    Added light and dark modes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1036995761"
+    serviceWorkerVersion: "3852084295"
   }
 });
